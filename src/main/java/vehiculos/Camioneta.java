50a490065441352735812fbd78dbe06f4eb0d633
Alejandro Giraldo <alejandro.giraldo@artifex.co>
2022-10-02 19:26:16 -0500

    Se corrige constructor

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -3,8 +3,8 @@ package vehiculos;
 public class Camioneta extends Vehiculo{
 	public Boolean volco;
 	
-	public Camioneta (String placa,Integer puertas, String nombre, Integer precio, Integer peso, Fabricante fabricante, Boolean volco) {
-		super(placa, puertas, 90, nombre, precio, peso, "4x4", fabricante);
+	public Camioneta (String placa, Integer puertas, String nombre, Integer precio, Integer peso, Fabricante fabricante, Boolean volco) {
+		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
 		cantidadCamionetas++;
 		CantidadVehiculos++;
